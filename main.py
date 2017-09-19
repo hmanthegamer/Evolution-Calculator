8ba919ab918fd67e7f6ffbc06fef8fa7036a9ca1
unknown <henry.balch1@marist.edu>
2017-09-19 16:08:48 -0400

    added help command. resolves #9

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ import pack, load, os
 def main():
     setup()
     while True:
-        x = input("GO: ")
+        x = str.lower(input("GO: "))
         if x == "add":
             cont = ""
             while cont == "":
                 cont = pack.main()
         elif x == "out":
             print(load.main())
+        #elif x == "list":
+        #    print(load.list_poke)
+        elif x == "help":
+            print("Commands:"
+                  "\nadd = regester a new pokemon"
+                  "\nout = output how many pokemon you can evolve")
+        else:
+            print("Try typing \"help\"")
 
 def setup():
     if not os.path.exists("storage"):
